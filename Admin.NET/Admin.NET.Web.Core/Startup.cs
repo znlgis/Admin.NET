ea112e421c5a592543c5312ef23174edf03e17ae
Lzh666 <422235757@qq.com>
2025-04-09 06:09:24 +0000

    请测试好再放上来，错误太明显了
    Signed-off-by: Lzh666 <422235757@qq.com>

--- Admin.NET/Admin.NET.Web.Core/Startup.cs
+++ Admin.NET/Admin.NET.Web.Core/Startup.cs
@@ -22,11 +22,9 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using OnceMi.AspNetCore.OSS;
 using SixLabors.ImageSharp.Web.DependencyInjection;
 using System;
-using System.Globalization;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Json;
@@ -87,7 +85,7 @@ public class Startup : AppStartup
         {
             setting.DateFormatHandling = DateFormatHandling.IsoDateFormat;
             setting.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-            setting.Converters.AddDateTimeTypeConverters(localized: true); // 时间本地化
+            //setting.Converters.AddDateTimeTypeConverters(localized: true); // 时间本地化
             setting.DateFormatString = "yyyy-MM-dd HH:mm:ss"; // 时间格式化
             setting.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; // 忽略循环引用
             // setting.ContractResolver = new CamelCasePropertyNamesContractResolver(); // 解决动态对象属性名大写
@@ -95,13 +93,8 @@ public class Startup : AppStartup
             // setting.Converters.AddLongTypeConverters(); // long转string（防止js精度溢出） 超过17位开启
             // setting.MetadataPropertyHandling = MetadataPropertyHandling.Ignore; // 解决DateTimeOffset异常
             // setting.DateParseHandling = DateParseHandling.None; // 解决DateTimeOffset异常
-            setting.Converters.Add(new IsoDateTimeConverter
-            {
-                DateTimeFormat = "yyyy-MM-dd HH:mm:ss", // 时间格式化
-                DateTimeStyles = DateTimeStyles.AssumeLocal | DateTimeStyles.AdjustToUniversal
-            }); // 解决DateTimeOffset异常
-        }
-        ;
+            // setting.Converters.Add(new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }); // 解决DateTimeOffset异常
+        };
 
         services.AddControllersWithViews()
             .AddAppLocalization()
@@ -112,7 +105,7 @@ public class Startup : AppStartup
             .AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All); // 禁止Unicode转码
-                options.JsonSerializerOptions.Converters.AddDateTimeTypeConverters("yyyy-MM-dd HH:mm:ss", localized: true); // 时间格式化
+                options.JsonSerializerOptions.Converters.AddDateTimeTypeConverters("yyyy-MM-dd HH:mm:ss"); // 时间格式化
             });
 
         // 三方授权登录OAuth
