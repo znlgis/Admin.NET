4fb0aa13740abeb446b68231d17fd4edec786a68
Lzh666 <422235757@qq.com>
2025-05-26 03:24:43 +0000

    调整租户站点信息为非必填，当填host时才校验必填；无站点配置取默认租户的
    Signed-off-by: Lzh666 <422235757@qq.com>

--- Admin.NET/Admin.NET.Core/Service/Tenant/Dto/TenantInput.cs
+++ Admin.NET/Admin.NET.Core/Service/Tenant/Dto/TenantInput.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -44,45 +44,42 @@ public class AddTenantInput : TenantOutput
     /// <summary>
     /// 系统主标题
     /// </summary>
-    [Required(ErrorMessage = "系统主标题不能为空")]
+    [CommonValidation("!string.IsNullOrWhiteSpace(Host) && string.IsNullOrWhiteSpace(Title)", "系统主标题不能为空")]
     public override string Title { get; set; }
 
     /// <summary>
     /// 系统副标题
     /// </summary>
-    [Required(ErrorMessage = "系统副标题不能为空")]
+    [CommonValidation("!string.IsNullOrWhiteSpace(Host) && string.IsNullOrWhiteSpace(ViceTitle)", "系统副标题不能为空")]
     public override string ViceTitle { get; set; }
 
     /// <summary>
     /// 系统描述
     /// </summary>
-    [Required(ErrorMessage = "系统描述不能为空")]
+    [CommonValidation("!string.IsNullOrWhiteSpace(Host) && string.IsNullOrWhiteSpace(ViceDesc)", "系统描述不能为空")]
     public override string ViceDesc { get; set; }
 
     /// <summary>
     /// 版权说明
     /// </summary>
-    [Required(ErrorMessage = "版权说明不能为空")]
+    [CommonValidation("!string.IsNullOrWhiteSpace(Host) && string.IsNullOrWhiteSpace(Copyright)", "版权说明不能为空")]
     public override string Copyright { get; set; }
 
     /// <summary>
     /// ICP备案号
     /// </summary>
-    [Required(ErrorMessage = "ICP备案号不能为空")]
     public override string Icp { get; set; }
 
     /// <summary>
     /// ICP地址
     /// </summary>
-    [Required(ErrorMessage = "ICP地址不能为空")]
+    [CommonValidation("!string.IsNullOrWhiteSpace(Host) && !string.IsNullOrWhiteSpace(Icp) && string.IsNullOrWhiteSpace(IcpUrl)", "ICP地址不能为空")]
     public override string IcpUrl { get; set; }
 
     /// <summary>
     /// Logo图片Base64码
     /// </summary>
-    [CommonValidation(
-        "string.IsNullOrWhiteSpace(Logo) && string.IsNullOrWhiteSpace(LogoBase64)", "图标不能为空"
-    )]
+    [CommonValidation("!string.IsNullOrWhiteSpace(Host) && string.IsNullOrWhiteSpace(Logo) && string.IsNullOrWhiteSpace(LogoBase64)", "图标不能为空")]
     public virtual string LogoBase64 { get; set; }
 
     /// <summary>
