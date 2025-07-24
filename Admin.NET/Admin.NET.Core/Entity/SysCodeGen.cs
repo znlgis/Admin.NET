0ad87a792afc12dc9ae4b9bca0d81077aef09215
松鼠 <hhhelong@126.com>
2025-07-24 07:52:45 +0000

    update Admin.NET/Admin.NET.Core/Entity/SysCodeGen.cs. Length 和MaxLength属性重复(AuthorName\TablePrefix等)或冲突(TableUniqueConfig 一个希望限制为 512，一个限制为 128); 属性默认值不清晰,为可空字符串提供默认值会更清晰，除非确实需要允许为空:public string AuthorName { get; set; } = "未知作者"; 添加数据库默认值约束:ColumnDescription = "是否生成菜单", DefaultValue = "1";
    Signed-off-by: 松鼠 <hhhelong@126.com>

--- Admin.NET/Admin.NET.Core/Entity/SysCodeGen.cs
+++ Admin.NET/Admin.NET.Core/Entity/SysCodeGen.cs
@@ -19,28 +19,24 @@ public partial class SysCodeGen : EntityBase
     /// 作者姓名
     /// </summary>
     [SugarColumn(ColumnDescription = "作者姓名", Length = 32)]
-    [MaxLength(32)]
-    public string? AuthorName { get; set; }
+    public string AuthorName { get; set; } = "未知作者"; 
 
     /// <summary>
     /// 是否移除表前缀
     /// </summary>
     [SugarColumn(ColumnDescription = "是否移除表前缀", Length = 8)]
-    [MaxLength(8)]
     public string? TablePrefix { get; set; }
 
     /// <summary>
     /// 生成方式
     /// </summary>
     [SugarColumn(ColumnDescription = "生成方式", Length = 32)]
-    [MaxLength(32)]
     public string? GenerateType { get; set; }
 
     /// <summary>
     /// 库定位器名
     /// </summary>
     [SugarColumn(ColumnDescription = "库定位器名", Length = 64)]
-    [MaxLength(64)]
     public string? ConfigId { get; set; }
 
     /// <summary>
@@ -68,55 +64,48 @@ public partial class SysCodeGen : EntityBase
     /// 数据库名(保留字段)
     /// </summary>
     [SugarColumn(ColumnDescription = "数据库库名", Length = 64)]
-    [MaxLength(64)]
     public string? DbName { get; set; }
 
     /// <summary>
     /// 数据库类型
     /// </summary>
     [SugarColumn(ColumnDescription = "数据库类型", Length = 64)]
-    [MaxLength(64)]
     public string? DbType { get; set; }
 
     /// <summary>
     /// 数据库链接
     /// </summary>
     [SugarColumn(ColumnDescription = "数据库链接", Length = 256)]
-    [MaxLength(256)]
     public string? ConnectionString { get; set; }
 
     /// <summary>
     /// 数据库表名
     /// </summary>
     [SugarColumn(ColumnDescription = "数据库表名", Length = 128)]
-    [MaxLength(128)]
     public string? TableName { get; set; }
 
     /// <summary>
     /// 命名空间
     /// </summary>
     [SugarColumn(ColumnDescription = "命名空间", Length = 128)]
-    [MaxLength(128)]
     public string? NameSpace { get; set; }
 
     /// <summary>
     /// 业务名
     /// </summary>
     [SugarColumn(ColumnDescription = "业务名", Length = 128)]
-    [MaxLength(128)]
     public string? BusName { get; set; }
 
     /// <summary>
     /// 表唯一字段配置
     /// </summary>
     [SugarColumn(ColumnDescription = "表唯一字段配置", Length = 512)]
-    [MaxLength(128)]
     public string? TableUniqueConfig { get; set; }
 
     /// <summary>
     /// 是否生成菜单
     /// </summary>
-    [SugarColumn(ColumnDescription = "是否生成菜单")]
+    [SugarColumn(ColumnDescription = "是否生成菜单"  DefaultValue = "1")]
     public bool GenerateMenu { get; set; } = true;
 
     /// <summary>
@@ -141,14 +130,12 @@ public partial class SysCodeGen : EntityBase
     /// 支持打印类型
     /// </summary>
     [SugarColumn(ColumnDescription = "支持打印类型", Length = 32)]
-    [MaxLength(32)]
     public string? PrintType { get; set; }
 
     /// <summary>
     /// 打印模版名称
     /// </summary>
     [SugarColumn(ColumnDescription = "打印模版名称", Length = 32)]
-    [MaxLength(32)]
     public string? PrintName { get; set; }
 
     /// <summary>
