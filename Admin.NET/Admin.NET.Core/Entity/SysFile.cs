1ed1a52de158aa88fea13ee45f1df0e3bf96a992
松鼠 <hhhelong@126.com>
2025-07-24 08:57:32 +0000

    update Admin.NET/Admin.NET.Core/Entity/SysFile.cs. Length和MaxLength 属性重复;
    Signed-off-by: 松鼠 <hhhelong@126.com>

--- Admin.NET/Admin.NET.Core/Entity/SysFile.cs
+++ Admin.NET/Admin.NET.Core/Entity/SysFile.cs
@@ -18,35 +18,30 @@ public partial class SysFile : EntityBaseTenantOrg
     /// 提供者
     /// </summary>
     [SugarColumn(ColumnDescription = "提供者", Length = 128)]
-    [MaxLength(128)]
     public string? Provider { get; set; }
 
     /// <summary>
     /// 仓储名称
     /// </summary>
     [SugarColumn(ColumnDescription = "仓储名称", Length = 128)]
-    [MaxLength(128)]
     public string? BucketName { get; set; }
 
     /// <summary>
     /// 文件名称（源文件名）
     /// </summary>
     [SugarColumn(ColumnDescription = "文件名称", Length = 128)]
-    [MaxLength(128)]
     public string? FileName { get; set; }
 
     /// <summary>
     /// 文件后缀
     /// </summary>
     [SugarColumn(ColumnDescription = "文件后缀", Length = 16)]
-    [MaxLength(16)]
     public string? Suffix { get; set; }
 
     /// <summary>
     /// 存储路径
     /// </summary>
     [SugarColumn(ColumnDescription = "存储路径", Length = 512)]
-    [MaxLength(512)]
     public string? FilePath { get; set; }
 
     /// <summary>
@@ -59,41 +54,36 @@ public partial class SysFile : EntityBaseTenantOrg
     /// 文件大小信息-计算后的
     /// </summary>
     [SugarColumn(ColumnDescription = "文件大小信息", Length = 64)]
-    [MaxLength(64)]
     public string? SizeInfo { get; set; }
 
     /// <summary>
     /// 外链地址-OSS上传后生成外链地址方便前端预览
     /// </summary>
     [SugarColumn(ColumnDescription = "外链地址", Length = 512)]
-    [MaxLength(512)]
     public string? Url { get; set; }
 
     /// <summary>
     /// 文件MD5
     /// </summary>
     [SugarColumn(ColumnDescription = "文件MD5", Length = 128)]
-    [MaxLength(128)]
     public string? FileMd5 { get; set; }
 
     /// <summary>
     /// 文件类别
     /// </summary>
     [SugarColumn(ColumnDescription = "文件类别", Length = 128)]
-    [MaxLength(128)]
     public virtual string? FileType { get; set; }
 
     /// <summary>
     /// 文件别名
     /// </summary>
     [SugarColumn(ColumnDescription = "文件别名", Length = 128)]
-    [MaxLength(128)]
     public string? FileAlias { get; set; }
 
     /// <summary>
     /// 是否公开
     /// </summary>
-    [SugarColumn(ColumnDescription = "是否公开")]
+    [SugarColumn(ColumnDescription = "是否公开"),IsNullable = false]
     public virtual bool IsPublic { get; set; } = false;
 
     /// <summary>
