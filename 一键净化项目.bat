b0ae9bde8ad5c9b75a0bd26273a5c853fe63c3f0
夜鹰 <liuxp1987@126.com>
2025-09-17 11:15:36 +0800

    同步代码

--- 一键净化项目.bat
+++ 一键净化项目.bat
@@ -1,11 +1,22 @@
 @echo OFF
- :begin
- REM 删除前端文件及文件夹
- DEL /f /s /q ".\Web\node_modules\*.*"
- RD /s /q ".\Web\node_modules"
- REM 循环删除指定文件夹下的文件夹
- FOR /d /r ".\Admin.NET\" %%b in (bin,obj,public) do rd /s /q "%%b"
- ECHO 【处理完毕，按任意键退出】
- PAUSE>NUL
- EXIT
- GOTO BEGIN
+setlocal enabledelayedexpansion
+
+echo 正在清理项目文件...
+
+REM 删除前端node_modules（更高效的方式）
+if exist ".\Web\node_modules" (
+    echo 正在删除 Web node_modules...
+    rd /s /q ".\Web\node_modules" 2>nul
+)
+
+REM 清理Admin.NET项目的bin、obj和public文件夹
+for /d /r ".\Admin.NET\" %%b in (bin obj public) do (
+    if exist "%%b" (
+        echo 正在删除 %%~b...
+        rd /s /q "%%b" 2>nul
+    )
+)
+
+echo 【处理完毕，按任意键退出】
+pause >nul
+exit /b 0
