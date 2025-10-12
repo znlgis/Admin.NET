bbd9ccf8190cddbf3f289b858f0d8d7aac88e025
高级CV工程师 <2535688890@qq.com>
2025-10-12 11:19:47 +0800

    去广告

--- Admin.NET/Admin.NET.Web.Core/Startup.cs
+++ Admin.NET/Admin.NET.Web.Core/Startup.cs
@@ -218,7 +218,7 @@ public class Startup : AppStartup
         services.AddCaptcha();
 
         // 控制台logo
-        services.AddConsoleLogo();
+        // services.AddConsoleLogo();
 
         // Swagger 时间格式化
         services.AddSwaggerGen(c =>
@@ -290,11 +290,11 @@ public class Startup : AppStartup
             app.UseHsts();
         }
 
-        app.Use(async (context, next) =>
-        {
-            context.Response.Headers.Append("Admin.NET", "Admin.NET");
-            await next();
-        });
+        // app.Use(async (context, next) =>
+        // {
+        //     context.Response.Headers.Append("Admin.NET", "Admin.NET");
+        //     await next();
+        // });
 
         // 图像处理
         app.UseImageSharp();
@@ -373,10 +373,10 @@ public class Startup : AppStartup
 
         app.UseInject(string.Empty, options =>
         {
-            foreach (var groupInfo in SpecificationDocumentBuilder.GetOpenApiGroups())
-            {
-                groupInfo.Description += "<br/><u><b><font color='FF0000'> 👮不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！</font></b></u>";
-            }
+            // foreach (var groupInfo in SpecificationDocumentBuilder.GetOpenApiGroups())
+            // {
+            //     groupInfo.Description += "<br/><u><b><font color='FF0000'> 👮不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！</font></b></u>";
+            // }
             options.ConfigureSwagger(m => {
                 m.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
             });
