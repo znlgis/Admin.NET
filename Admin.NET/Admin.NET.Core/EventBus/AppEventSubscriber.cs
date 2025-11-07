ebea40fb44db0bda1eba57fb7b617f4b74d7ddd3
夜鹰 <liuxp1987@126.com>
2025-11-08 01:27:59 +0800

    同步代码

--- Admin.NET/Admin.NET.Core/EventBus/AppEventSubscriber.cs
+++ Admin.NET/Admin.NET.Core/EventBus/AppEventSubscriber.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -27,7 +27,10 @@ public class AppEventSubscriber : IEventSubscriber, ISingleton, IDisposable
     [EventSubscribe(CommonConst.AddExLog)]
     public async Task CreateExLog(EventHandlerExecutingContext context)
     {
-        var db = _serviceScope.ServiceProvider.GetRequiredService<ISqlSugarClient>();
+        // 切换日志独立数据库
+        var db = SqlSugarSetup.ITenant.IsAnyConnection(SqlSugarConst.LogConfigId)
+            ? SqlSugarSetup.ITenant.GetConnectionScope(SqlSugarConst.LogConfigId)
+            : SqlSugarSetup.ITenant.GetConnectionScope(SqlSugarConst.MainConfigId);
         await db.CopyNew().Insertable((SysLogEx)context.Source.Payload).ExecuteCommandAsync();
     }
 
